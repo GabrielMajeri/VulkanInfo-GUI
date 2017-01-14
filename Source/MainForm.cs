fb30f5af291077104ae63dd8461805a1b7ea3097
Gabriel Majeri <gabriel.majeri6@gmail.com>
2017-01-14 20:36:01 +0200

    Cleanup MSBuild project

---
+++ Source/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Forms;
+using System.Diagnostics;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace VulkanInfo_GUI
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private async void GetInfoButton_ClickAsync(object sender, EventArgs e)
+		{
+			string fileNotFoundMessage = "Could not find VulkanInfo, make sure the Vulkan Runtime is installed.";
+			string fileNotFoundCaption = "File not found";
+
+			string vkInfoOutput = string.Empty;
+
+			using (var vkInfoProc = new Process())
+			{
+				vkInfoProc.StartInfo.UseShellExecute = false;
+				vkInfoProc.StartInfo.RedirectStandardOutput = true;
+				vkInfoProc.StartInfo.CreateNoWindow = true;
+
+				vkInfoProc.StartInfo.FileName = "cmd.exe";
+
+				string vkInfoExeName = Environment.Is64BitOperatingSystem ? "vulkaninfo.exe" : "vulkaninfo32.exe";
+				vkInfoProc.StartInfo.Arguments = "/c \"" + vkInfoExeName + "\"";
+				
+				try
+				{
+					vkInfoProc.Start();
+
+					getInfoButton.Enabled = false;
+
+					infoTextBox.Text = await vkInfoProc.StandardOutput.ReadToEndAsync();
+
+					vkInfoProc.Dispose();
+
+					getInfoButton.Enabled = false;
+					saveToFileButton.Enabled = true;
+				}
+				catch(Win32Exception)
+				{
+					MessageBox.Show(fileNotFoundMessage,
+						fileNotFoundCaption,
+						MessageBoxButtons.OK,
+						MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		private void SaveToFileButton_Click(object sender, EventArgs e)
+		{
+			if(saveInfoDialog.ShowDialog() == DialogResult.OK)
+				File.WriteAllText(saveInfoDialog.FileName, infoTextBox.Text);
+		}
+	}
+}
---
+++ Source/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VulkanInfo_GUI
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			var screenSize = Screen.PrimaryScreen.WorkingArea;
+			
+			var warningMessage = "Your primary monitor's resolution is less than 800x600, and is therefore unsupported." + 
+				"You might not be able to see all the information properly.";
+			var warningCaption = "Warning - Monitor resolution is too small";
+
+			if (screenSize.Width < 800 || screenSize.Height < 600)
+				MessageBox.Show(warningMessage, warningCaption, MessageBoxButtons.OK);
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+	}
+}
